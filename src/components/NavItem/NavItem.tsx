2b19e9e9d706ad71ea6bc1501978488fa0aeffa5
Gee Chai <fc00004@gmail.com>
2024-10-17 13:25:26 +0900

    styles: faint border bottom added to navItem when sidebar is present increase visibility of navItems

--- src/components/NavItem/NavItem.tsx
+++ src/components/NavItem/NavItem.tsx
@@ -13,13 +13,12 @@ const NavItem: React.FC<NavItemProps> = ({
   variant,
   closeSidebar,
 }) => {
-  // Define the Tailwind styles for each variant
   const getClassName = () => {
     switch (variant) {
       case "header":
         return "px-3 py-1 mx-2 text-base hover:text-blue-500 transition duration-200 ease-in-out";
       case "sidebar":
-        return "block px-6 py-4 text-lg font-medium border-b transition-all duration-300 ease-in-out";
+        return "block px-6 py-4 text-lg font-medium border-b border-[var(--sidebar-border-color)] transition-all duration-300 ease-in-out";
       default:
         return "";
     }
