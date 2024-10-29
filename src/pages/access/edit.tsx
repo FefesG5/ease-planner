d4dc78d9a4a61974412ff1cb01771240201c68a5
Gee Chai <fc00004@gmail.com>
2024-10-29 11:16:03 +0900

    feat: simplfied Edit page wrapped withDashboardLayout

--- src/pages/access/edit.tsx
+++ src/pages/access/edit.tsx
@@ -1,30 +1,12 @@
-import React from "react";
-import { useAuthContext } from "@/contexts/AuthContext";
-import Spinner from "@/components/Spinner/Spinner";
-import DashboardLayout from "@/components/Dashboard/DashboardLayout";
+import withDashboardLayout from "@/hoc/withDashboardLayout";
 
-export default function Edit() {
-  const { user, loading, isAuthorized, signOutUser } = useAuthContext();
-
-  // Handle loading state
-  if (loading) {
-    return <Spinner />;
-  }
-
-  // If no user is logged in or the user is not authorized, redirect or show message
-  if (!user) {
-    return <p>You must be logged in to access this page.</p>;
-  }
-
-  if (!isAuthorized) {
-    return <p>You are not authorized to access this page.</p>;
-  }
-
-  // Render the content if user is authenticated and authorized
+function Edit() {
   return (
-    <DashboardLayout user={user} signOutUser={signOutUser}>
+    <div>
       <h1>Review & Edit</h1>
       <p>Review and edit your schedules here.</p>
-    </DashboardLayout>
+    </div>
   );
 }
+
+export default withDashboardLayout(Edit);
