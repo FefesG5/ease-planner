cfc719f26cbd7360d1ee70fdd1bbc21a2253f474
Gee Chai <fc00004@gmail.com>
2024-10-31 11:39:40 +0900

    feat: help page added to the access page

---
+++ src/pages/access/help.tsx
@@ -0,0 +1,12 @@
+import withDashboardLayout from "@/hoc/withDashboardLayout";
+
+function Help() {
+  return (
+    <div>
+      <h1>Help</h1>
+      <p>Useful Information</p>
+    </div>
+  );
+}
+
+export default withDashboardLayout(Help);
