d417fe8ab103fe703e2be62ba8f1824360ee1375
Gee Chai <fc00004@gmail.com>
2024-11-01 09:53:39 +0900

    styles: reduce sign out button size

--- src/components/SignOutButton/SignOutButton.tsx
+++ src/components/SignOutButton/SignOutButton.tsx
@@ -6,7 +6,7 @@ const SignOutButton: React.FC<SignOutButtonProps> = ({ signOutUser }) => {
   return (
     <button
       onClick={signOutUser}
-      className="bg-[var(--signin-btn-bg-color)] text-[var(--signin-text-color)] border-none py-2 px-4 md:py-3 md:px-6 text-sm md:text-base font-bold rounded-lg cursor-pointer transition-colors duration-300 ease-in-out hover:bg-[var(--signin-btn-hover-bg-color)]"
+      className="bg-[var(--signin-btn-bg-color)] text-[var(--signin-text-color)] border-none py-2 px-4 md:py-1.5 md:px-3 text-xs font-medium rounded cursor-pointer transition-colors duration-200 ease-in-out hover:bg-[var(--signin-btn-hover-bg-color)]"
     >
       Sign Out
     </button>
