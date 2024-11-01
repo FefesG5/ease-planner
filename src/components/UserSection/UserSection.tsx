2acdb9ba5be43ffa6b8e78106951113e2fc39fb8
Gee Chai <fc00004@gmail.com>
2024-11-01 09:54:55 +0900

    styles: reduce padding, margin in the user section, reduce email and user name size

--- src/components/UserSection/UserSection.tsx
+++ src/components/UserSection/UserSection.tsx
@@ -9,20 +9,20 @@ interface UserSectionProps {
 
 const UserSection: React.FC<UserSectionProps> = ({ user, signOutUser }) => {
   return (
-    <div className="flex items-center justify-between p-3 bg-[var(--user-section-bg-color)] border-b border-gray-300 shadow-md">
-      <div className="flex items-center space-x-4">
+    <div className="flex items-center justify-between p-4 bg-[var(--user-section-bg-color)] border-b border-gray-300 shadow-md">
+      <div className="flex items-center space-x-3">
         <Image
           src={user.photoURL || "/user-icon.svg"}
           alt={`${user.displayName || "User"}'s profile`}
-          width={40}
-          height={40}
-          className="rounded-full border-2 border-gray-300 shadow-sm"
+          width={36}
+          height={36}
+          className="rounded-full border border-gray-300 shadow-sm"
         />
         <div className="flex flex-col">
-          <h3 className="text-lg font-semibold text-[var(--body-text-color)]">
+          <h3 className="text-base font-medium text-[var(--body-text-color)]">
             {user.displayName || "User"}
           </h3>
-          <p className="text-sm text-[var(--body-text-color)]">{user.email}</p>
+          <p className="text-xs text-[var(--body-text-color)]">{user.email}</p>
         </div>
       </div>
       <SignOutButton signOutUser={signOutUser} />
