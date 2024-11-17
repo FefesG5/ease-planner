35836b02247a84a8984098dffa1a9aa01ce68741
Gee Chai <fc00004@gmail.com>
2024-11-17 20:29:03 +0900

    chore: teachersName added to data as a temp solution for providing users name

---
+++ src/data/teachersName.ts
@@ -0,0 +1,21 @@
+// Temporary Solution until a feteching request for all the users can be created
+
+export const teacherNames = [
+  "Ari",
+  "Chai",
+  "Rachel",
+  "Itxy",
+  "Yuri",
+  "Zoe",
+  "Atsumi",
+  "Michelle",
+  "Sora",
+  "Ayaka",
+  "Yoshi K",
+  "Haruki",
+  "Sota",
+  "Shivani",
+  "Yuki",
+  "Haruto",
+  "Mamiko",
+];
