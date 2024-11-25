1b1f7640fe923d80d565e8a23176a2394dc646ac
Gee Chai <fc00004@gmail.com>
2024-11-25 15:27:09 +0900

    styles: convert inline styles to tailwind styles

--- src/components/AttendanceHeaderSection/AttendanceHeaderSection.tsx
+++ src/components/AttendanceHeaderSection/AttendanceHeaderSection.tsx
@@ -1,170 +1,59 @@
+import React from "react";
+
 const AttendanceHeader = () => {
   return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "1fr 1fr 1fr",
-        alignItems: "center",
-        backgroundColor: "white",
-      }}
-    >
+    <div className="grid grid-cols-3 items-center bg-white">
       {/* Year Month */}
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "flex-start",
-          height: "100%",
-          paddingLeft: "20px",
-          fontWeight: "bold",
-        }}
-      >
+      <div className="flex items-center justify-start h-full pl-5 font-bold">
         2024年10月度
       </div>
 
       {/* Title */}
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          height: "100%",
-          fontSize: "1.5rem",
-          fontWeight: "bold",
-        }}
-      >
+      <div className="flex items-center justify-center h-full text-xl font-bold">
         出 勤 簿
       </div>
       {/* Empty Space to leave space on the right */}
       <div />
 
       {/* Main Content Section */}
-      <div
-        style={{
-          gridColumn: "1 / 4",
-          display: "grid",
-          gridTemplateColumns: "3fr 5fr 2fr",
-          gap: "0",
-        }}
-      >
+      <div className="grid grid-cols-[3fr_5fr_2fr] col-span-3 gap-0 mt-0">
         {/* Place Section */}
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "auto 1fr",
-            alignItems: "stretch",
-            border: "1px solid black",
-            height: "100%",
-          }}
-        >
+        <div className="grid grid-cols-[auto_1fr] items-stretch border border-black h-full">
           {/* School Title */}
-          <div
-            style={{
-              borderRight: "1px solid black",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              height: "100%",
-              padding: "0 5px",
-            }}
-          >
+          <div className="border-r border-black flex items-center justify-center h-full px-1.5">
             所属
           </div>
           {/* School Name */}
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              height: "100%",
-              padding: "0 5px",
-            }}
-          >
+          <div className="flex items-center h-full px-1.5">
             TryAngle Kids 南草津校
           </div>
         </div>
 
         {/* Name Section */}
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "1fr 3fr",
-            alignItems: "stretch",
-            border: "1px solid black",
-            height: "100%",
-          }}
-        >
+        <div className="grid grid-cols-[1fr_3fr] items-stretch border border-black h-full">
           {/* Name Labels Container */}
-          <div
-            style={{
-              display: "grid",
-              gridTemplateRows: "1fr 1fr",
-              borderRight: "1px solid black",
-              height: "100%",
-            }}
-          >
+          <div className="grid grid-rows-2 border-r border-black h-full">
             {/* Name Title English */}
-            <div
-              style={{
-                borderBottom: "1px solid black",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                height: "100%",
-                padding: "0 5px",
-              }}
-            >
+            <div className="border-b border-black flex items-center justify-center h-full px-1.5">
               Name
             </div>
             {/* Name Title Japanese */}
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                fontSize: "0.875rem",
-                height: "100%",
-                padding: "0 5px",
-              }}
-            >
+            <div className="flex items-center justify-center h-full text-sm px-1.5">
               氏名
             </div>
           </div>
           {/* Name Box Area */}
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              height: "100%",
-              padding: "0 5px",
-            }}
-          >
-            Gee Chai
-          </div>
+          <div className="flex items-center h-full px-1.5">Gee Chai</div>
         </div>
 
         {/* Signature Section */}
-        <div
-          style={{
-            display: "grid",
-            gridTemplateRows: "1fr 1fr",
-            border: "1px solid black",
-            marginLeft: "20px",
-            marginRight: "20px",
-          }}
-        >
+        <div className="grid grid-rows-2 border border-black ml-5 mr-5">
           {/* Signature Title */}
-          <div
-            style={{
-              textAlign: "left",
-              borderBottom: "1px solid black",
-              paddingLeft: "5px",
-              paddingRight: "5px",
-            }}
-          >
+          <div className="text-left border-b border-black px-1.5">
             Signature
           </div>
           {/* Empty box area */}
-          <div style={{ padding: "5px" }}></div>
+          <div className="px-1.5"></div>
         </div>
 
         {/* Empty Space */}
