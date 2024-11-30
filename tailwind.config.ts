9dfa6997fe6d065225f536cbe851dd98ce67da8b
Gee Chai <fc00004@gmail.com>
2024-11-30 23:43:14 +0900

    chore: custom css for padding values added to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      spacing: {
+        // Add custom small padding values here
+        '0.1': '0.025rem', // 0.1 translates to ~0.4px
+        '0.2': '0.05rem',  // 0.2 translates to ~0.8px
+        '0.3': '0.075rem', // 0.3 translates to ~1.2px
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
