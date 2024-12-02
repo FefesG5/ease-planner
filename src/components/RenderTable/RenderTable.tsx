b309803883359ba2207774720c79cf423f62b94f
Gee Chai <fc00004@gmail.com>
2024-12-02 11:38:38 +0900

    styles: adjust certain text size and margin size to better display a excel styles format

--- src/components/RenderTable/RenderTable.tsx
+++ src/components/RenderTable/RenderTable.tsx
@@ -23,38 +23,38 @@ const RenderTable: React.FC<RenderTableProps> = ({
         schoolName={schoolName}
         teacherName={teacherName}
       />
-      <table className="w-full border-collapse border border-black text-[6px] sm:text-[7px] md:text-[8px] lg:text-[9px] xl:text-[10px] mt-2 sm:mt-4">
+      <table className="w-full border-collapse border border-black text-[6px] sm:text-[7px] md:text-[8px] lg:text-[7px] xl:text-[8px] mt-2 sm:mt-4">
         <thead>
           {/* English Headers Row */}
           <tr>
-            <th className="border border-black text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-2 lg:py-2 xl:px-2 xl:py-1 font-normal">
+            <th className="border border-black text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-1 lg:py-1 xl:px-1 xl:py-1 font-normal">
               date
             </th>
-            <th className="border border-black text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-2 lg:py-2 xl:px-2 xl:py-1 font-normal whitespace-nowrap">
+            <th className="border border-black text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-1 lg:py-1 xl:px-1 xl:py-1 font-normal whitespace-nowrap">
               day
             </th>
-            <th className="border border-black text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-2 lg:py-2 xl:px-2 xl:py-1 font-normal">
+            <th className="border border-black text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-1 lg:py-1 xl:px-1 xl:py-1 font-normal">
               starting time
             </th>
-            <th className="border border-black text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-2 lg:py-2 xl:px-2 xl:py-1 font-normal">
+            <th className="border border-black text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-1 lg:py-1 xl:px-1 xl:py-1 font-normal">
               finishing time
             </th>
-            <th className="border border-black text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-2 lg:py-2 xl:px-2 xl:py-1 font-normal">
+            <th className="border border-black text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-1 lg:py-1 xl:px-1 xl:py-1 font-normal">
               overtime
             </th>
-            <th className="border border-black text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-2 lg:py-2 xl:px-2 xl:py-1 font-normal">
+            <th className="border border-black text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-1 lg:py-1 xl:px-1 xl:py-1 font-normal">
               break time
             </th>
-            <th className="border border-black text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-2 lg:py-2 xl:px-2 xl:py-1 font-normal">
+            <th className="border border-black text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-1 lg:py-1 xl:px-1 xl:py-1 font-normal">
               working hours
             </th>
-            <th className="border border-black text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-2 lg:py-2 xl:px-2 xl:py-1 font-normal">
+            <th className="border border-black text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-1 lg:py-1 xl:px-1 xl:py-1 font-normal">
               lesson hours
             </th>
-            <th className="border border-black text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-2 lg:py-2 xl:px-2 xl:py-1 font-normal">
+            <th className="border border-black text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-1 lg:py-1 xl:px-1 xl:py-1 font-normal">
               non lesson hours
             </th>
-            <th className="border border-black text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-2 lg:py-2 xl:px-2 xl:py-1 font-normal">
+            <th className="border border-black text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-1 lg:py-1 xl:px-1 xl:py-1 font-normal">
               approval
             </th>
           </tr>
@@ -63,7 +63,7 @@ const RenderTable: React.FC<RenderTableProps> = ({
             {table.getHeaderGroups()[0].headers.map((header) => (
               <th
                 key={header.id}
-                className="border border-black font-normal text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-2 lg:py-2 xl:px-2 xl:py-1 whitespace-nowrap"
+                className="border border-black font-normal text-center px-0.25 py-0.25 sm:px-0.5 sm:py-0.5 md:px-1 md:py-1 lg:px-1 lg:py-1 xl:px-2 xl:py-1 whitespace-nowrap"
               >
                 {header.isPlaceholder
                   ? null
