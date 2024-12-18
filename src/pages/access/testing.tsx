f52e7721fb494ffc0ba988fb98fceb5d0b0ed87d
Gee Chai <fc00004@gmail.com>
2024-12-18 15:23:16 +0900

    feat: new testing page added to develop new Edit page

---
+++ src/pages/access/testing.tsx
@@ -0,0 +1,12 @@
+import withDashboardLayout from "@/hoc/withDashboardLayout";
+
+function Testing() {
+  return (
+    <div>
+      <h1>Testing</h1>
+      <p>Bad Testing</p>
+    </div>
+  );
+}
+
+export default withDashboardLayout(Testing);
