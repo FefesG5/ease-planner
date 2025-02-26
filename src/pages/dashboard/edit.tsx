1992ce377b654d2de2c8429bc552c8d101ecdd24
Gee Chai <fc00004@gmail.com>
2025-02-26 14:32:19 +0900

    styles: school table button color added with gray border to outline table section

--- src/pages/dashboard/edit.tsx
+++ src/pages/dashboard/edit.tsx
@@ -287,7 +287,7 @@ function Edit() {
                   [school]: !prev[school],
                 }))
               }
-              className="w-full text-left px-2 py-1 font-medium border-b text-lg bg-[var(--signin-input-bg-color)]"
+              className="w-full text-left px-2 py-1 font-medium text-lg bg-[var(--schedule-list-bg-color)] border border-gray-100"
             >
               {schoolStates[school] ? "▼" : "►"} School {school}
             </button>
